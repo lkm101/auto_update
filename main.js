898ef861fa32c68d685aafc692a507dfd90edad4
Lee KiMok <70103652+lkm101@users.noreply.github.com>
2024-03-28 23:26:41 +0900

    주요 기능 주석 설명 추가

--- main.js
+++ main.js
@@ -6,15 +6,19 @@ const path = require('path')
 let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
+  // 윈도우 브라우저 초기 셋팅
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    // frame: false,
+    // minWidth: 100, // 최소 가로
+    // minHeight: 50, // 최소 세로
     webPreferences: {
-      // preload: path.join(__dirname, 'preload.js')
+      /**
+       * TypeError: window.require is not a function 일때 추가
+       * electron에 빌드된 react 같이 개발하기 위해서 false 로 설정
+       */
+      nodeIntegrationInWorker: true,
       nodeIntegration: true,
-      contextIsolation: false,
     }
   })
 
@@ -24,7 +28,7 @@ function createWindow () {
     mainWindow = null;
   });
 
-  // Open the DevTools.
+  // 개발자 툴 실행
   // mainWindow.webContents.openDevTools()
 }
 
@@ -34,75 +38,75 @@ app.on('ready', ()=>{
   // autoUpdater.checkForUpdates();
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-// app.whenReady().then(() => {
-//   createWindow()
+// Electron 초기화 마치고 브라우처 창을 생성할 준비가 되면 호출
+app.whenReady().then(() => {
+  createWindow()
+}
 
-//  app.on('activate', function () {
-//     // On macOS it's common to re-create a window in the app when the
-//     // dock icon is clicked and there are no other windows open.
-//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-//   })
-// })
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// 완전히 종료 될 때 호출
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
+// 앱이 실행될 때 기본 프로세스 정의
 app.on('activate', function(){
   if(mainWindow === null){
     createWindow();
   }
 });
 
-
+// 앱 버전 리턴하는 리스너
 ipcMain.on('app_version', (event)=>{
   try{
-    console.log("app.getVersion()", app.getVersion());
-    // event.reply('app_version', {version: app.getVersion()});
     event.sender.send('app_version', {version: app.getVersion()});
-  
-    // autoUpdater.checkForUpdatesAndNotify();
-    // console.log("autoUpdater.checkForUpdatesAndNotify();")
   }
   catch(err){
     console.log("err", err);
   }
 });
 
+// 1. 신규 버전 릴리즈 확인 시 호출
 autoUpdater.on("checking-for-update", () => {
-  // sendStatusToWindow("Checking for update...");
   mainWindow.webContents.send('app_version', {version: "Checking for update..."});
 });
 
+// 업데이트 할 신규 버전이 없을 시 호출
 autoUpdater.on("update-not-available", (info) => {
   mainWindow.webContents.send('app_version', {version: "Update not available."});
-  // sendStatusToWindow("Update not available.");
-});
-autoUpdater.on("error", (err) => {
-  mainWindow.webContents.send('app_version', {version: "Error in auto-updater. " + err});
-  // sendStatusToWindow("Error in auto-updater. " + err);
 });
 
+// 업데이트 프로세스 중 에러 방생 시 호출
+autoUpdater.on("error", (err) => {
+  mainWindow.webContents.send('app_version', {version: "Error in auto-updater. " + err});
+});
+
+// 2. 업데이트 할 신규 버전이 있을 시 호출 됨
 autoUpdater.on('update-available', ()=>{
   console.log("update-available");
   mainWindow.webContents.send('update_available');
 });
 
+// 3. 업데이트 설치 파일 다운로드 상태 수신
+// 1~3 단계까지 자동으로 진행 됨
+autoUpdater.on('download-progress', progressObj => {
+  let percent = progressObj.percent.toFixed(2);
+  console.log(`다운로드중... ${percent}%(${progressObj.transferred}/${progressObj.total})`);
+});
+
+// 업데이트 설치 파일 다운로드 완료 시 업데이트 진행 여부 선택
 autoUpdater.on('update-downloaded', ()=>{
   console.log("update_downloaded");
   mainWindow.webContents.send('update_downloaded');
-});
 
-ipcMain.on('restart_app', ()=>{
-  console.log("restart_app");
-  autoUpdater.quitAndInstall();
+  const option = {
+    type: 'info',
+    buttons: ['업데이트'],
+    defaultId: 0,
+    title: '업데이트 마법사',
+    message: '업데이트 버튼을 클릭하여 업데이트를 진행해주세요.'
+  };
+
+  dialog.showMessageBox(option).then(function (res) {
+    autoUpdater.quitAndInstall();
+  });
 });
